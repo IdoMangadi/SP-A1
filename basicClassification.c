37f3ee5b79bef58d819eae0073a82e5e45cca1da
IdoMangadi <idomangadi@gmail.com>
2024-01-20 22:57:26 +0200

    fixing isStrong

--- basicClassification.c
+++ basicClassification.c
@@ -10,44 +10,19 @@ int isStrong (int x)
         return 0;
     }
 
-    double y = (double)x;
-    int j = 1;
-    int digitsCounter = 0;
-    while(TRUE)
-    {    
-        if((y/j) >= 1)
-        {
-            digitsCounter++;
-            j = j * 10;
-        }
-        else
-        {
-            break;
-        }
-    }
-
-    int num = x;
+    int num = x; //a number to work on
     int sum = 0;
-    int factorialSum = 1;
-    int digit;
-    j = 10;
-    for(int k=0; k<digitsCounter; k++)
-    {
-        digit = (num % j) / (j / 10);
-
-        for(int i=0; i<=digit-1; i++)
+    while(num > 0)
+    {    
+        int digit = num % 10;
+        int factorialSum = 1;
+        for(int i=1; i<=digit; i++)
         {
-            factorialSum = factorialSum * (i+1);
+            factorialSum = factorialSum * i;
         }
         sum += factorialSum;
-        factorialSum = 1;
-        j = j * 10;
-
+        num = num / 10;
     }
 
-    if((sum -x) == 0)
-    {
-        return 1;
-    }
-    return 0;
+    return (sum == x);
 }
