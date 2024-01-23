7396c9ab7388716d6797b7632412da809eaf38df
IdoMangadi <idomangadi@gmail.com>
2024-01-23 19:33:21 +0200

    finished

--- main.c
+++ main.c
@@ -5,54 +5,54 @@ int main()
 {
     //printf("Enter the first number: ");
     int num1;
-    scanf("%d", &num1);
-    //printf("Enter the second number: ");
-    int num2;
-    scanf("%d", &num2);
-    
-    if(num1 > num2)
-    {
-        int tmp = num1;
-        num1 = num2;
-        num2 = tmp;
-    }
+    if(scanf("%d", &num1) == 1){
+        //printf("Enter the second number: ");
+        int num2;
+        if(scanf("%d", &num2) == 1){
+        
+            if(num1 > num2)
+            {
+                int tmp = num1;
+                num1 = num2;
+                num2 = tmp;
+            }
 
-    //Printing:
-    printf("The Armstrong numbers are:");
-    for(int i=num1; i<=num2; i++)
-    {
-        if(isArmstrong(i))
-        {
-            printf(" %d", i);
+            //Printing:
+            printf("The Armstrong numbers are:");
+            for(int i=num1; i<=num2; i++)
+            {
+                if(isArmstrong(i))
+                {
+                    printf(" %d", i);
+                }
+            }
+            printf("\nThe Palindromes are:");
+            for(int i=num1; i<=num2; i++)
+            {
+                if(isPalindrome(i))
+                {
+                    printf(" %d", i);
+                }
+            }
+            printf("\nThe Prime numbers are:");
+            for(int i=num1; i<=num2; i++)
+            {
+                if(isPrime(i))
+                {
+                    printf(" %d", i);
+                }
+            }
+            printf("\nThe Strong numbers are:");
+            for(int i=num1; i<=num2; i++)
+            {
+                if(isStrong(i))
+                {
+                    printf(" %d", i);
+                }
+            }
+            printf("\n");
+
+            return  0;
         }
     }
-    printf("\nThe Palindromes are:");
-    for(int i=num1; i<=num2; i++)
-    {
-        if(isPalindrome(i))
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\nThe Prime numbers are:");
-    for(int i=num1; i<=num2; i++)
-    {
-        if(isPrime(i))
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\nThe Strong numbers are:");
-    for(int i=num1; i<=num2; i++)
-    {
-        if(isStrong(i))
-        {
-            printf(" %d", i);
-        }
-    }
-    printf("\n");
-
-    return  0;
-
-
 }
